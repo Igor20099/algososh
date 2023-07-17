e723c75b8d6008b7c65748c83427d1ba93a50b74
Igor <megakill123@yandex.ru>
2023-07-17 23:21:20 +0500

    edit utils.ts and edit fibonacci-page.tsx

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import styles from "./fibonacci-page.module.css";
+import { delay,getFibArray } from "../../utils/utils";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import { ElementStates } from "../../types/element-states";
+
 
 export const FibonacciPage: React.FC = () => {
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+  const [num, setNum] = useState<number>(0);
+  const [circleNumArray, setCircleNumArray] = useState<Array<number>>([]);
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
+
+  const numberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (
+      e.target.value === "" ||
+      Number(e.target.value) > 19 ||
+      Number(e.target.value) <= 0
+    ) {
+      setButtonDisabled(true);
+    } else {
+      setButtonDisabled(false);
+    }
+    setNum(Number(e.target.value));
+  };
+
+  
+
+  const handleButton = async () => {
+    setIsLoad(true);
+    const arr = getFibArray(num)
+    for (let i = 0; i < arr.length; i++) {
+      await delay(SHORT_DELAY_IN_MS);
+      setCircleNumArray(arr.slice(0, i + 1));
+    }
+    setIsLoad(false);
+  };
+
+  const justifyStyle =
+  circleNumArray && circleNumArray.length < 10
+      ? { justifyContent: "center" }
+      : { justifyContent: "flex-start" };
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <div className={styles.wrapper}>
+        <Input
+          isLimitText={true}
+          max={19}
+          type="number"
+          onChange={numberChange}
+        />
+        <Button
+          text="Рассчитать"
+          extraClass={styles.button}
+          onClick={handleButton}
+          isLoader={isLoad}
+          disabled={buttonDisabled}
+        />
+      </div>
+      <div className={styles.circles} style={justifyStyle}>
+        {circleNumArray &&
+          circleNumArray.map((el, i) => {
+            return (
+              <Circle
+                key={i}
+                letter={el.toString()}
+                state={ElementStates.Default}
+                index={i}
+                extraClass={styles.circe}
+              />
+            );
+          })}
+      </div>
     </SolutionLayout>
   );
 };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,3 +11,11 @@ export async function delay(milliseconds:number) {
         setTimeout(resolve,milliseconds)
     })
 }
+
+export const getFibArray = (n: number) : number[] => {
+    let arr: number[] = [1, 1];
+    for (let i = 2; i < n + 1; i++) {
+      arr.push(arr[i - 2] + arr[i - 1]);
+    }
+    return arr;
+  };
