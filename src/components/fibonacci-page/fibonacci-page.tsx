64e489354c07b1a8f6d9a2d6e92fec811c384ef3
Igor <megakill123@yandex.ru>
2023-07-25 22:21:25 +0500

    edit project

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -4,10 +4,9 @@ import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import styles from "./fibonacci-page.module.css";
-import { delay,getFibonacciNumbers } from "../../utils/utils";
+import { delay, getFibonacciNumbers } from "../../utils/utils";
 import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 import { ElementStates } from "../../types/element-states";
-
 
 export const FibonacciPage: React.FC = () => {
   const [isLoad, setIsLoad] = useState<boolean>(false);
@@ -28,11 +27,9 @@ export const FibonacciPage: React.FC = () => {
     setNum(Number(e.target.value));
   };
 
-  
-
   const handleButton = async () => {
     setIsLoad(true);
-    const arr = getFibonacciNumbers(num)
+    const arr = getFibonacciNumbers(num);
     for (let i = 0; i < arr.length; i++) {
       await delay(SHORT_DELAY_IN_MS);
       setCircleNumArray(arr.slice(0, i + 1));
@@ -41,7 +38,7 @@ export const FibonacciPage: React.FC = () => {
   };
 
   const justifyStyle =
-  circleNumArray && circleNumArray.length < 10
+    circleNumArray && circleNumArray.length < 10
       ? { justifyContent: "center" }
       : { justifyContent: "flex-start" };
 
