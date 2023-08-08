f35cc12115035fb603c470096fc5f5401b573a83
Igor <megakill123@yandex.ru>
2023-08-08 20:56:06 +0500

    edit string.tsx

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Dispatch, SetStateAction } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
@@ -9,6 +9,34 @@ import { StringData } from "../../types/string-data";
 import { delay, swap } from "../../utils/utils";
 import { DELAY_IN_MS } from "../../constants/delays";
 
+export const reverseWord = async (
+  arr: StringData[],
+  setData: Dispatch<SetStateAction<StringData[]>>,
+  setIsLoad: Dispatch<SetStateAction<boolean>>
+) => {
+  setIsLoad(true);
+  const mid = Math.ceil(arr.length / 2);
+
+  for (let i = 0; i < mid; i++) {
+    let j = arr.length - 1 - i;
+
+    if (i !== j) {
+      arr[i].state = ElementStates.Changing;
+      arr[j].state = ElementStates.Changing;
+      setData([...arr]);
+      await delay(DELAY_IN_MS);
+    }
+
+    swap(arr, i, j);
+
+    arr[i].state = ElementStates.Modified;
+    arr[j].state = ElementStates.Modified;
+
+    setData([...arr]);
+  }
+  setIsLoad(false);
+};
+
 export const StringComponent: React.FC = () => {
   const [isLoad, setIsLoad] = useState<boolean>(false);
   const [data, setData] = useState<StringData[]>([]);
@@ -18,35 +46,11 @@ export const StringComponent: React.FC = () => {
     setWord(e.target.value);
   };
 
-  const reverseWord = async (arr: StringData[]) => {
-    setIsLoad(true);
-    const mid = Math.ceil(arr.length / 2);
-
-    for (let i = 0; i < mid; i++) {
-      let j = arr.length - 1 - i;
-
-      if (i !== j) {
-        arr[i].state = ElementStates.Changing;
-        arr[j].state = ElementStates.Changing;
-        setData([...arr]);
-        await delay(DELAY_IN_MS);
-      }
-
-      swap(arr, i, j);
-
-      arr[i].state = ElementStates.Modified;
-      arr[j].state = ElementStates.Modified;
-
-      setData([...arr]);
-    }
-    setIsLoad(false);
-  };
-
   const handleButton = () => {
     const newArr = word
       .split("")
       .map((value) => ({ value, state: ElementStates.Default }));
-    reverseWord(newArr);
+    reverseWord(newArr,setData,setIsLoad);
   };
 
   return (
